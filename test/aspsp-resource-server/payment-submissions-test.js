292f2e9ec61e29fbd503b440f5f55cf9571560c6
robmckinnon <rob@movingflow>
2017-12-01 16:06:04 +0000

    Add payment submissions middleware
    Add tests for payments middleware and payment submissions middleware.

--- test/aspsp-resource-server/payment-submissions-test.js
+++ test/aspsp-resource-server/payment-submissions-test.js
@@ -1,5 +1,15 @@
+const express = require('express');
 const assert = require('assert');
+const request = require('supertest'); // eslint-disable-line
 const { postPaymentSubmissionsResponse } = require('../../lib/aspsp-resource-server/payment-submissions.js');
+const { paymentSubmissionsMiddleware } = require('../../lib/aspsp-resource-server');
+const { requestPayload } = require('./payments-test.js');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(paymentSubmissionsMiddleware);
 
 const paymentId = '123';
 const paymentSubmissionId = '456';
@@ -14,5 +24,28 @@ describe('postPaymentsResponse', () => {
     assert.equal(data.PaymentSubmissionId, paymentSubmissionId);
     assert.equal(data.PaymentId, paymentId);
     assert.equal(data.Status, status);
+  });
+});
+
+describe('paymentsMiddleware', () => {
+  process.env.VERSION = 'v1.1';
+  const payload = () => {
+    requestPayload.Data = Object.assign(requestPayload.Data, { PaymentId: paymentId });
+    return requestPayload;
+  };
+
+  it('handles /payments request', (done) => {
+    request(app)
+      .post('/open-banking/v1.1/payment-submissions')
+      .set('authorization', `Bearer ${process.env.ACCESS_TOKEN}`)
+      .send(payload())
+      .end((err, res) => {
+        assert.equal(res.status, 201);
+        const data = res.body.Data;
+        assert.ok(data.PaymentId);
+        assert.ok(data.PaymentSubmissionId);
+        assert.deepEqual(data.Status, 'AcceptedSettlementInProcess');
+        done();
+      });
   });
 });
--- test/aspsp-resource-server/payments-test.js
+++ test/aspsp-resource-server/payments-test.js
@@ -1,8 +1,17 @@
+const express = require('express');
 const assert = require('assert');
+const request = require('supertest'); // eslint-disable-line
 const { postPaymentsResponse } = require('../../lib/aspsp-resource-server/payments.js');
+const { paymentsMiddleware } = require('../../lib/aspsp-resource-server');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(paymentsMiddleware);
 
 const risk = {};
-const request = {
+const requestPayload = {
   Data: {
     Initiation: {
       InstructionIdentification: 'ACME412',
@@ -26,14 +35,35 @@ describe('postPaymentsResponse', () => {
   it('creates response based on request', () => {
     const paymentId = '123';
     const status = 'AcceptedTechnicalValidation';
-    const response = postPaymentsResponse(request, paymentId, status);
+    const response = postPaymentsResponse(requestPayload, paymentId, status);
     const data = response.Data;
     assert.ok(data.CreationDateTime);
 
     assert.equal(data.PaymentId, paymentId);
     assert.equal(data.Status, status);
 
-    assert.deepEqual(data.Initiation, request.Data.Initiation);
+    assert.deepEqual(data.Initiation, requestPayload.Data.Initiation);
     assert.deepEqual(response.Risk, risk);
   });
 });
+
+describe('paymentsMiddleware', () => {
+  process.env.VERSION = 'v1.1';
+
+  it('handles /payments request', (done) => {
+    request(app)
+      .post('/open-banking/v1.1/payments')
+      .set('authorization', `Bearer ${process.env.ACCESS_TOKEN}`)
+      .send(requestPayload)
+      .end((err, res) => {
+        assert.equal(res.status, 201);
+        const data = res.body.Data;
+        assert.ok(data.PaymentId);
+        assert.deepEqual(data.Status, 'AcceptedTechnicalValidation');
+        assert.deepEqual(data.Initiation, requestPayload.Data.Initiation);
+        done();
+      });
+  });
+});
+
+exports.requestPayload = requestPayload;
